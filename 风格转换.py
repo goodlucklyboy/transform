6b40dbe25ca3ebf674658609de91ad492616d215
yang peng <1181121206@qq.com>
2019-03-03 10:35:07 +0800

    官网上面的版本，能运行。官方给的例子，效果很好，但是用上自己的图片，不尽如人意。

--- 风格转换.py
+++ 风格转换.py
@@ -1,7 +1,8 @@
 from __future__ import print_function
 
-import torch as t
+import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from torch.autograd import Variable
 import torch.optim as optim
 
@@ -11,221 +12,248 @@ import torchvision.transforms as transforms
 import torchvision.models as models
 
 import copy
-use_cuda = torch.cuda.is_available()
-dtype = torch.cuda.FloatTensor if use_cuda else torch.FloatTensor
-imsize = 512 if use_cuda else 128
-loader = transforms.Compose([transforms.Scale(imsize),transforms.ToTensor()])
-style_img_path = 'style.jpg'
-content_img_path = 'content.jpg'
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# desired size of the output image
+imsize = 400 if torch.cuda.is_available() else 128  # use small size if no gpu
+
+loader = transforms.Compose([
+    transforms.Resize((imsize,imsize)),  # scale imported image
+    transforms.ToTensor()])  # transform it into a torch tensor
+
+
 def image_loader(image_name):
-	image = Image.open(image_name)
-	image = Variable(loader(image))
-	image = image.unsqueeze(0)
-	return image
+    image = Image.open(image_name)
+    # fake batch dimension required to fit network's input dimensions
+    image = loader(image).unsqueeze(0)
+    return image.to(device, torch.float)
 
 
-style_img = image_loader(style_img_path).type(dtype)
-content_img = image_loader(content_img_path).type(dtype)
+style_img = image_loader("./data/style.jpg")
+content_img = image_loader("./data/content.jpg")
 
-assert style_img.size() == content_img.size()
 
-# ***********************************************************************
-#Display images
+assert style_img.size() == content_img.size(), \
+    "we need to import style and content images of the same size"
 
-unloader = transforms.ToPILImage()
+unloader = transforms.ToPILImage()  # reconvert into PIL image
+
 plt.ion()
 
+def imshow(tensor, title=None):
+    image = tensor.cpu().clone()  # we clone the tensor to not do changes on it
+    image = image.squeeze(0)      # remove the fake batch dimension
+    image = unloader(image)
+    plt.imshow(image)
+    if title is not None:
+        plt.title(title)
+    plt.pause(1) # pause a bit so that plots are updated
 
-def figure_imshow(tensor,title=None):
-	image = tensor.cpu()
-	image = image.view(3,imsize,imsize)
-	image = unloader(image)
-	plt.imshow(image)
-	if title is not None:
-		plt.title(title)
-	plt.pause(1)
-	
-	
-plt.figure()
-figure_imshow(style_img.data, title='Style Image')
 
 plt.figure()
-figure_imshow(content_img.data, title='Content Image')
+imshow(style_img, title='Style Image')
 
-def GramMatrix(input):
-	a, b, c, d = input.size()
-	features = input.view(a*b, c*d)
-	G = torch.mm(features, features.t())
-	return G.div(a*b*c*d)
-
-	
-	
-"""      定义ContentLoss类         """
+plt.figure()
+imshow(content_img, title='Content Image')
 
 
 class ContentLoss(nn.Module):
-	def __init__(self,target,weight):
-		super(ContentLoss,self).__init__()
-		self.target = target.detach()*weight
-		self.weight = weight
-		self.criterion = nn.MSELoss()
-		self.loss = 0
-		self.output = 0
-		
-	def forward(self,input):
-		self.loss = self.criterion(input * self.weight, self.target)
-		self.output = input
-		return self.output
-	
-	
-"""      定义StyleLoss类       """
+
+    def __init__(self, target,):
+        super(ContentLoss, self).__init__()
+        # we 'detach' the target content from the tree used
+        # to dynamically compute the gradient: this is a stated value,
+        # not a variable. Otherwise the forward method of the criterion
+        # will throw an error.
+        self.target = target.detach()
+
+    def forward(self, input):
+        self.loss = F.mse_loss(input, self.target)
+        return input
+    
+
+def gram_matrix(input):
+    a, b, c, d = input.size()  # a=batch size(=1)
+    # b=number of feature maps
+    # (c,d)=dimensions of a f. map (N=c*d)
+
+    features = input.view(a * b, c * d)  # resise F_XL into \hat F_XL
+
+    G = torch.mm(features, features.t())  # compute the gram product
+
+    # we 'normalize' the values of the gram matrix
+    # by dividing by the number of element in each feature maps.
+    return G.div(a * b * c * d)
 
 
-class StyleLoss(nn.Module):#这个类的定义目前有问题，且看后面如何使用这个类来返回查看
-	
-	def __init__(self, target, weight):
-		super(StyleLoss,self).__init__()
-		self.target = target.detach()*weight
-		self.weight = weight
-		self.gram = GramMatrix()
-		self.criterion = nn.MSELoss()
-	
-	def forward(self, input):
-		self.output = input.clone()
-		self.gram_matrix= self.gram(input)
-		self.gram_matrix.mul_(self.weight)
-		loss = self.criterion(self.gram_matrix,self.target)
-		return self.output
-	
-	def backward(self,retain_graph=True):
-		self.loss.backward(retain_graph=retain_graph)
-		return self.loss
-	
+class StyleLoss(nn.Module):
 
-	
-"""Load the neural network"""
+    def __init__(self, target_feature):
+        super(StyleLoss, self).__init__()
+        self.target = gram_matrix(target_feature).detach()
+
+    def forward(self, input):
+        G = gram_matrix(input)
+        self.loss = F.mse_loss(G, self.target)
+        return input
+    
+cnn = models.vgg19(pretrained=True).features.to(device).eval()
 
 
-cnn = models.vgg19(pretrained=True).features
+cnn_normalization_mean = torch.tensor([0.485, 0.456, 0.406]).to(device)
+cnn_normalization_std = torch.tensor([0.229, 0.224, 0.225]).to(device)
 
-if use_cuda:
-	cnn = cnn.cuda()
-	
+# create a module to normalize input image so we can easily put it in a
+# nn.Sequential
+class Normalization(nn.Module):
+    def __init__(self, mean, std):
+        super(Normalization, self).__init__()
+        # .view the mean and std to make them [C x 1 x 1] so that they can
+        # directly work with image Tensor of shape [B x C x H x W].
+        # B is batch size. C is number of channels. H is height and W is width.
+        self.mean = torch.tensor(mean).view(-1, 1, 1)
+        self.std = torch.tensor(std).view(-1, 1, 1)
+
+    def forward(self, img):
+        # normalize img
+        return (img - self.mean) / self.std
+    
+    
 content_layers_default = ['conv_4']
 style_layers_default = ['conv_1', 'conv_2', 'conv_3', 'conv_4', 'conv_5']
 
-
-def get_style_model_and_losses(net, style_img, content_img, style_weight=1000,
-                               content_weight=1, content_layers=content_layers_default,
+def get_style_model_and_losses(cnn, normalization_mean, normalization_std,
+                               style_img, content_img,
+                               content_layers=content_layers_default,
                                style_layers=style_layers_default):
-	cnn = copy.deepcopy(net)
-	content_losses = []
-	style_losses = []
-	model = nn.Sequential()
-	if use_cuda:
-		model = model.cuda()
-	i = 1
-	for layer in list(cnn):
-		if isinstance(layer,nn.Conv2d):
-			name = "conv"+str(i)
-			model.add_module(name,layer)
-		
-			if name in content_layers:
-				target = model(content_img).clone()
-				content_loss = ContentLoss(target,content_weight)
-				model.add_module("content_loss"+str(i),content_loss)
-				content_losses.append(content_loss)
-			
-			if name in style_layers:
-				target_feature = model(style_img).clone()
-				target_feature_gram = GramMatrix(target_feature)
-				style_loss = StyleLoss(target_feature_gram,style_weight)
-				model.add_module("style_loss"+str(i), style_loss)
-				style_losses.append(style_loss)
-		
-		if isinstance(layer,nn.ReLU):
-			name = "relu"+str(i)
-			model.add_module(name, layer)
-			
-			if name in content_layers:
-				target = model(content_img).clone()
-				content_loss = ContentLoss(target,content_weight)
-				model.add_module("content_loss"+str(i),content_loss)
-				content_losses.append(content_loss)
-				
-			if name in style_layers:
-				target_feature = model(style_img).clone()
-				target_feature_gram = GramMatrix(target_feature)
-				style_loss = StyleLoss(target_feature_gram,style_weight)
-				model.add_module("style_loss"+str(i), style_loss)
-				style_losses.append(style_loss)
-			
-			i += 1
-		if isinstance(layer,nn.MaxPool2d):
-			name = "pool_"+str(i)
-			model.add_module(name,layer)
-	
-	return model,style_losses,content_losses
+    cnn = copy.deepcopy(cnn)
 
+    # normalization module
+    normalization = Normalization(normalization_mean, normalization_std).to(device)
 
-"""input image"""
+    # just in order to have an iterable access to or list of content/syle
+    # losses
+    content_losses = []
+    style_losses = []
+
+    # assuming that cnn is a nn.Sequential, so we make a new nn.Sequential
+    # to put in modules that are supposed to be activated sequentially
+    model = nn.Sequential(normalization)
+
+    i = 0  # increment every time we see a conv
+    for layer in cnn.children():
+        if isinstance(layer, nn.Conv2d):
+            i += 1
+            name = 'conv_{}'.format(i)
+        elif isinstance(layer, nn.ReLU):
+            name = 'relu_{}'.format(i)
+            # The in-place version doesn't play very nicely with the ContentLoss
+            # and StyleLoss we insert below. So we replace with out-of-place
+            # ones here.
+            layer = nn.ReLU(inplace=False)
+        elif isinstance(layer, nn.MaxPool2d):
+            name = 'pool_{}'.format(i)
+        elif isinstance(layer, nn.BatchNorm2d):
+            name = 'bn_{}'.format(i)
+        else:
+            raise RuntimeError('Unrecognized layer: {}'.format(layer.__class__.__name__))
+
+        model.add_module(name, layer)
+
+        if name in content_layers:
+            # add content loss:
+            target = model(content_img).detach()
+            content_loss = ContentLoss(target)
+            model.add_module("content_loss_{}".format(i), content_loss)
+            content_losses.append(content_loss)
+
+        if name in style_layers:
+            # add style loss:
+            target_feature = model(style_img).detach()
+            style_loss = StyleLoss(target_feature)
+            model.add_module("style_loss_{}".format(i), style_loss)
+            style_losses.append(style_loss)
+
+    # now we trim off the layers after the last content and style losses
+    for i in range(len(model) - 1, -1, -1):
+        if isinstance(model[i], ContentLoss) or isinstance(model[i], StyleLoss):
+            break
+
+    model = model[:(i + 1)]
+
+    return model, style_losses, content_losses
+
 
 input_img = content_img.clone()
-figure_imshow(input_img.data,title='input image')
+# if you want to use white noise instead uncomment the below line:
+# input_img = torch.randn(content_img.data.size(), device=device)
 
-
-def get_input_param_optimizer(input_img):
-	input_param = nn.Parameter(input_img.data)
-	optimizer = optim.LBFGS([input_param])
-	return input_param, optimizer
-
-def run_style_transfer(cnn, content_img, style_img, input_img, num_steps=1000,
-                       style_weight= 1000, content_weight=1):
-
-	print('Buiding the style transfer model..')
-	model, style_losses, content_losses = get_style_model_and_losses(cnn, style_img,
-	                                                                 content_img,style_weight, content_weight)
-	input_param, optimizer = get_input_param_optimizer(input_img)
-	print('Optimizing..')
-	run = [0]
-	while run[0]<= num_steps:
-		def closure():
-			input_param.data.clamp_(0, 1)
-			
-			optimizer.zero_grad()
-			model(input_param)
-			style_score = 0
-			content_score = 0
-			
-			for s1 in style_losses:
-				style_score += s1.backward()
-			for c1 in content_losses:
-				content_score += c1.backward()
-				
-			run[0] += 1
-			if run[0]%50 == 0:
-				print("run{}:".format(run))
-				print('Style Loss:{:4f} '
-				      'ContentLoss:{:4f}'.format(style_score,content_score))
-			
-			return style_score+content_score
-		optimizer.step(closure)
-		
-		
-	input_param.data.clamp_(0,1)
-	return input_param.data
-
-
-output = run_style_transfer(cnn,content_img,style_img,input_img)
+# add the original input image to the figure:
 plt.figure()
-figure_imshow(output,title='OUTPUT iamge')
+imshow(input_img, title='Input Image')
 
+def get_input_optimizer(input_img):
+    # this line to show that input is a parameter that requires a gradient
+    optimizer = optim.LBFGS([input_img.requires_grad_()])
+    return optimizer
+
+
+def run_style_transfer(cnn, normalization_mean, normalization_std,
+                       content_img, style_img, input_img, num_steps=200,
+                       style_weight=1000000, content_weight=1):
+    """Run the style transfer."""
+    print('Building the style transfer model..')
+    model, style_losses, content_losses = get_style_model_and_losses(cnn,
+        normalization_mean, normalization_std, style_img, content_img)
+    optimizer = get_input_optimizer(input_img)
+
+    print('Optimizing..')
+    run = [0]
+    while run[0] <= num_steps:
+
+        def closure():
+            # correct the values of updated input image
+            input_img.data.clamp_(0, 1)
+
+            optimizer.zero_grad()
+            model(input_img)
+            style_score = 0
+            content_score = 0
+
+            for sl in style_losses:
+                style_score += sl.loss
+            for cl in content_losses:
+                content_score += cl.loss
+
+            style_score *= style_weight
+            content_score *= content_weight
+
+            loss = style_score + content_score
+            loss.backward()
+
+            run[0] += 1
+            if run[0] % 50 == 0:
+                print("run {}:".format(run))
+                print('Style Loss : {:4f} Content Loss: {:4f}'.format(
+                    style_score.item(), content_score.item()))
+                print()
+
+            return style_score + content_score
+
+        optimizer.step(closure)
+
+    # a last correction...
+    input_img.data.clamp_(0, 1)
+
+    return input_img
+
+
+output = run_style_transfer(cnn, cnn_normalization_mean, cnn_normalization_std,
+                            content_img, style_img, input_img)
+
+plt.figure()
+imshow(output, title='Output Image')
+
+# sphinx_gallery_thumbnail_number = 4
 plt.ioff()
 plt.show()
-
-	
-				
-				
-				
-				
-		
